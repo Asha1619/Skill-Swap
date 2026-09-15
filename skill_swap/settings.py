714cb8b504736131bef9579a670b6a0bbe73e616
Asha R <asharadhakrishnan1619@gmail.com>
2026-09-15 15:02:44 +0530

    Update database name in settings.py

--- skill_swap/settings.py
+++ skill_swap/settings.py
@@ -74,16 +74,29 @@ WSGI_APPLICATION = 'skill_swap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'skillswapdb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'asha',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'skillswapdb',
-        'USER': 'postgres',
-        'PASSWORD': 'asha',
+        'NAME':'Ekart db',
+        'USER':'postgres',
+        'PASSWORD':'asha',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT':'5432',
     }
-}
+    ]
 
 
 # Password validation
